ab50c77d206fc9e762e01297234bec3f47f4c40b
devpraveen001 <praveenseenu629@gmail.com>
2026-03-05 22:26:35 +0530

    Create drive-config.js

---
+++ public/src/js/config/drive-config.js
@@ -0,0 +1,11 @@
+export const GOOGLE_DRIVE_CONFIG = {
+    CLIENT_ID: '1060370205325-s0rq68mhv2gtjrivvvs064mrf0q0u4e2.apps.googleusercontent.com',
+    API_KEY: 'YOUR_API_KEY_HERE',
+    DISCOVERY_DOCS: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+    SCOPES: 'https://www.googleapis.com/auth/drive.file'
+};
+
+export const OAUTH_TOKENS = {
+    access_token: 'ya29.a0ATkoCc6u8XrwEKu2kqtkglm9r_ecWHf0nzAU8P2HFyRcth3Nz2FuTX3ozxvesQP9Oi9N81s_xgYZHTpoExyM2X40oGjoTzv0v4p86AnkxYJhTkTpzke3Jr7q7ZhYU_qqlH7U1Pr3lvXTKVjFhzDC9aH-1nZ-JDDetG74wKlhEnP7nvmirI9LAKOk6tcdAn-HXRoJxRAaCgYKAVMSARASFQHGX2MihXYHUBlLn62GRneX9FYpNA0206',
+    refresh_token: '1//04ARCqEdu5vHuCgYIARAAGAQSNwF-L9Ir913RjVeGEhHuiUWLwurluaQmoHu_hhYIqF_s44fzYq0Cy31yrQ_DHqODFACHgcod-2I'
+};
