1c8f7171517fddce589a49e20d532348b6620356
devpraveen001 <praveenseenu629@gmail.com>
2026-03-06 23:58:37 +0530

    Update Google Drive API Client ID and tokens

--- public/src/js/services/google-drive.js
+++ public/src/js/services/google-drive.js
@@ -1,5 +1,5 @@
 // Google Drive API Integration
-const CLIENT_ID = '1060370205325-s0rq68mhv2gtjrivvvs064mrf0q0u4e2.apps.googleusercontent.com'; // Replace with your actual Client ID
+const CLIENT_ID = '488447923830-1j5qdrosg6t56e5d01fsbtepr5f11fnn.apps.googleusercontent.com'; // Replace with your actual Client ID
 const API_KEY = 'AIzaSyD9tS0rLdPz9t7906aW9wLsKkU4xWl6i8g'; // Replace with your actual API Key
 const DISCOVERY_DOCS = ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'];
 const SCOPES = 'https://www.googleapis.com/auth/drive.file';
@@ -7,8 +7,8 @@ const SCOPES = 'https://www.googleapis.com/auth/drive.file';
 let tokenClient;
 let gapiInited = false;
 let gisInited = false;
-let accessToken = 'ya29.a0ATkoCc6u8XrwEKu2kqtkglm9r_ecWHf0nzAU8P2HFyRcth3Nz2FuTX3ozxvesQP9Oi9N81s_xgYZHTpoExyM2X40oGjoTzv0v4p86AnkxYJhTkTpzke3Jr7q7ZhYU_qqlH7U1Pr3lvXTKVjFhzDC9aH-1nZ-JDDetG74wKlhEnP7nvmirI9LAKOk6tcdAn-HXRoJxRAaCgYKAVMSARASFQHGX2MihXYHUBlLn62GRneX9FYpNA0206';
-let refreshToken = '1//04ARCqEdu5vHuCgYIARAAGAQSNwF-L9Ir913RjVeGEhHuiUWLwurluaQmoHu_hhYIqF_s44fzYq0Cy31yrQ_DHqODFACHgcod-2I';
+let accessToken = 'ya29.a0ATkoCc7_X9mkrIcP0fUxuEud5lIKO2ZFeT2ZEu-GtWh0PP9y3SN4V45Eebx-jufjHhsDR5lQpARE1lypLDPvIOjMu2cgLRBJW8a61PCOtsLzRfgw8xp3jc5B2a0dvcJGUxAvta_yc6xuOpaGH9XhzJngpNhMgUTT8lvMsQVEWkhuhifnHeAk0fx6mz4bFoIy-oUtFPYaCgYKATcSARISFQHGX2MiRB3krsXJe2CcrkxE6Su1tA0206';
+let refreshToken = '1//04KAkx4X4QspNCgYIARAAGAQSNwF-L9IrzroFhZNJC3UWibMjPKvgWyzpN1L_r0fqlXf3ct8LpXASGUZY-n18LBzfSxYp2gwiB_I';
 
 // Initialize Google API client
 export async function initGoogleDrive() {
